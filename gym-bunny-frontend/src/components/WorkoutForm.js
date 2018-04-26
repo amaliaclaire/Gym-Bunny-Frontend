1b408db390ddb93f77c8abbb16cdf364c05ed675
Amalia Claire Fergin <acfergin@msn.com>
2018-04-25 17:08:06 -0700

    setting up create app

--- gym-bunny-frontend/src/components/WorkoutForm.js
+++ gym-bunny-frontend/src/components/WorkoutForm.js
@@ -4,19 +4,40 @@ import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 class WorkoutForm extends React.Component {
   constructor(props) {
     super(props);
+    state = {
+      workout_name: ''
+    }
+
+    handleChange = event => {
+      this.setState({ workout_name: event.target.value });
+    }
+
+    handleSubmit = event => {
+      event.preventDefault();
+
+      const workoutSingleForm = {
+        workout_name: this.state.workout_name
+      }
+
+    }
     // proptypes blah blah
 
-
+    axios.post('http://', {workout_name})
+    .then(res => {
+      console.log(res);
+      console.log(res.data);
+    })
   }
+
   render () {
     return (
       <div>
-        <Form>
+        <Form onSubmit={this.handleSubmit}>
           <FormGroup>
             <Label>Name of Workout</Label>
-            <Input type="text" name="workout-name" id="workform-name" />
+            <Input type="text" name="workout-name" id="workform-name" onChange={this.handleChange} />
           </FormGroup>
-          <Button onClick={this.props.onCreateWorkout}>Create Workout</Button>
+          <Button type="submit">Create Workout</Button>
         </Form>
       </div>
     );
