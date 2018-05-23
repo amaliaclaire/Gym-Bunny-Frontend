7854b4f0a19d87ceb697907c6cacaf4743b0539b
Amalia Claire Fergin <acfergin@msn.com>
2018-05-23 13:39:01 -0700

    login front end complete

--- gym-bunny-frontend/src/components/Login.js
+++ gym-bunny-frontend/src/components/Login.js
@@ -1,18 +1,38 @@
 import React from 'react';
-
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
 class Login extends React.Component {
   constructor (props) {
     super(props)
+
+    this.state = {
+      username: '',
+      password: ''
+    }
   }
 
   render () {
     return (
       <div>
-        <h1>Login</h1>
+          <MuiThemeProvider>
+            <div>
+            <AppBar title="Login"/>
+            <TextField hintText= "Enter your username" floatingLabelText="Username" onChange = {(event, newValue) => this.setState ({username: newValue})}/>
+            <br/>
+            <TextField hintText="Enter your password" floatingLabelText="password" onChange = {(event, newValue) => this.setState ({password: newValue})} />
+            <br/>
+            <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)} />
+            </div>
+          </MuiThemeProvider>
       </div>
-    )
+    );
   }
 }
+const style = {
+  margin: 15,
+}; 
 
 export default Login
