ca06553d5a7d4cb3d6400318b5cc3f14510138e0
Amalia Claire Fergin <acfergin@msn.com>
2018-05-23 13:03:02 -0700

    login screen, login and sign up are printed to the front end'

--- gym-bunny-frontend/src/App.js
+++ gym-bunny-frontend/src/App.js
@@ -7,6 +7,9 @@ import WorkoutForm from './components/WorkoutForm'
 import ExerciseForm from './components/ExerciseForm'
 import CompletedWorkout from './components/CompletedWorkout'
 import Exercises from './components/Exercises'
+import Login from './components/Login'
+import LoginScreen from './components/LoginScreen'
+import Register from './components/Register'
 import { Route, Link, Switch } from 'react-router-dom'
 
 
@@ -15,7 +18,10 @@ const Pages = {
   ExerciseForm: 'exercise form',
   WorkoutForm: 'workout form',
   Workouts: 'workouts',
-  Exercises: 'exercises'
+  Exercises: 'exercises',
+  Login: 'login',
+  LoginScreen: 'LoginScreen',
+  Register: 'register'
 
 
 }
@@ -56,12 +62,18 @@ class App extends Component {
         <Link to='/exerciseForm'></Link>
         <Link to='/workout'></Link>
         <Link to='/workoutsWithExercises'></Link>
+        <Link to='/login'></Link>
+        <Link to='/loginScreen'></Link>
+        <Link to='/register'></Link>
         <Switch>
         <Route exact path='/' component={ImageCard}/>
         <Route path='/workoutForm' component={WorkoutForm} />
         <Route path='/exerciseForm' component={ExerciseForm} />
         <Route path='/workout' component={CompletedWorkout} />
         <Route path='/workoutsWithExercises' component={Exercises} />
+        <Route path='/login' component={Login} />
+        <Route path='/loginScreen' component={LoginScreen} />
+        <Route path='/register' component={Register} />
         </Switch>
       </div>
     );
---
+++ gym-bunny-frontend/src/components/LoginScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class LoginScreen extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+        <h1>Login Screen</h1>
+      </div>
+    )
+  }
+}
+
+export default LoginScreen;
